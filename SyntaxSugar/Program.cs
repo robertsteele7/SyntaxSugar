dbfdde54a81ce075ba340b770b11ace155f5725b
robertsteele7 <atpcycle7@gmail.com>
2022-08-25 11:40:29 -0500

    first commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 11;
+            var response = (answer < 9) ? "{answer} is less than nine" : "{answer} is greater than nine";
+          
+        }
+    }
+}
